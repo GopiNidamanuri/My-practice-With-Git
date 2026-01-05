a412b7a7770613fc881b6f036656758a3289307a
Gopi Nidamanuri <gopinidamanuri99@gmail.com>
2026-01-05 18:58:19 +0530

    Add project files.

---
+++ My practice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace My_practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
